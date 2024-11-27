435ea8cb47758d2ce033e30d00119f8f0a4766bc
AntonioOliveiraa <antoniooliveiraantonio-augusto@outlook.com>
2024-11-27 18:48:40 -0300

    Set base servo

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ void setup() {
   myservo2.attach(10); // attaches the servo on pin 10 to the servo object
   myservo3.write(170); // sets initial position
   myservo3.attach(11); // attaches the servo on pin 11 to the servo object
-  myservo4.write(90); // sets initial position
+  myservo4.write(100); // sets initial position
   myservo4.attach(6); // attaches the servo on pin 6 to the servo object
   Serial.begin(9600);
 }
