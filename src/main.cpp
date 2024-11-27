ccccceb383587ceea4ab203ac865fb1529aad04c
AntonioOliveiraa <antoniooliveiraantonio-augusto@outlook.com>
2024-11-27 15:54:13 -0300

    Sets da garra e ombro

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,9 @@ void setup() {
   myservo1.attach(9);  // attaches the servo on pin 9 to the servo object
   myservo2.write(90);  // sets initial position
   myservo2.attach(10); // attaches the servo on pin 10 to the servo object
+  myservo3.write(170); // sets initial position
   myservo3.attach(11); // attaches the servo on pin 11 to the servo object
+  myservo4.write(90); // sets initial position
   myservo4.attach(6); // attaches the servo on pin 6 to the servo object
   Serial.begin(9600);
 }
